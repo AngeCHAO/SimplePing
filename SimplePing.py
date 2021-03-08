f492232c497ddd55f37fa2187953a7d513ba66a7
AngeCHAO <79137186+AngeCHAO@users.noreply.github.com>
2021-03-08 20:43:25 +0800

    Add files via upload

--- SimplePing.py
+++ SimplePing.py
@@ -1,9 +1,9 @@
-import os
+import os  #導入OS模塊到當前程序
+
 ip_list = ["8.8.8.8", "8.8.4.4", "192.168.1.1"]
 for ip in ip_list:
-...     response = os.popen(f"ping {ip}").read()
-...     if "Received = 4" in response:
-...         print(f"UP {ip} Ping Successful")
-...     else:
-...         print(f"DOWN {ip} Ping Unsuccssful")
-
+        response = os.popen(f"ping {ip}").read()  #response 像伺服器傳送OS popen對象的屬性的ip
+        if "Received =4" in response:
+            print(f"UP {ip} Ping Successful")
+        else:
+            print(f"DOWN {ip} Ping Unsuccessful")
